582953b44da15963c5054861b1f0c275eb90cf13
John Rae <jrae15@ford.com>
2024-07-25 11:25:08 +0100

    stuff

--- src/main/java/com/oocode/HomePage.java
+++ src/main/java/com/oocode/HomePage.java
@@ -5,7 +5,7 @@ public class HomePage {
             <html>
                 <body>
                 <h1>Extreme startup player</h1>
-                <p>This is a minimal web app starting point for <a href='https://extreme-startup.fly.dev/'>Extreme startup</a></p>
+                <p>Hi there, This is a minimal web app starting point for <a href='https://extreme-startup.fly.dev/'>Extreme startup</a></p>
                 </body>
             </html>""".trim();
 }
